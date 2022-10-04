534a22e3b2403741fcf43f5fd95e90d04d3f4aca
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-10-05 01:06:27 +0300

    Completed Assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Actors Page</h1>
+    {actors.map((movie)=> (
+    <div key={movie.name}>
+    {movie.name}
+    
+    <ul>
+      {movie.movies.map((name)=><li key={name}>{name}</li>)}
+    </ul>
+    </div>))}
+  </div>
+  )
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Directors Page</h1>
+    {directors.map((movie) => 
+    <div key={movie.name}>
+{movie.name}
+<ul>
+  {movie.movies.map((movy) => (
+    <li key={movy}>{movy}</li>
+  ))}
+</ul>
+    </div>
+    )}
+    
+  </div>;
 }
 
 export default Directors;
